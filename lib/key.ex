2c3ced4c2abd680bafbcaafa5f5db0a402318574
Sam Yeo <yeosamuel95@gmail.com>
2021-04-26 04:40:42 -0600

    ✨ Add function to create seed randomly as string

--- lib/key.ex
+++ lib/key.ex
@@ -1,3 +1,14 @@
 defmodule Key do
+  def create_seed(seed \\ "") do
+    random_single_digit =
+      Enum.random(0..9)
+      |> Integer.to_string()
+    seed = seed <> random_single_digit
 
+    if String.length(seed) < 5 do
+      create_seed(seed)
+    else
+      seed
+    end
+  end
 end
