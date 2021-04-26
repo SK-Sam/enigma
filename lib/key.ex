ff1f439954b90f4cc8ca9fcea2bd92fc2242a074
Sam Yeo <yeosamuel95@gmail.com>
2021-04-26 04:56:23 -0600

    ✨ Create function to get ABCD keys

--- lib/key.ex
+++ lib/key.ex
@@ -1,4 +1,13 @@
 defmodule Key do
+  defstruct a: nil, b: nil, c: nil, d: nil
+
+  def get_abcd_keys(seed) do
+    all_keys = %Key{}
+    all_keys = %{all_keys | a: String.slice(seed, 0..1) }
+    all_keys = %{all_keys | b: String.slice(seed, 1..2) }
+    all_keys = %{all_keys | c: String.slice(seed, 2..3) }
+    %{all_keys | d: String.slice(seed, 3..4) }
+  end
   def create_seed(seed \\ "") do
     random_single_digit =
       Enum.random(0..9)
