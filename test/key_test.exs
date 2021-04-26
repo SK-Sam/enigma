3e3258a5ea42555d8bc4379e3d1a68886f677f05
Sam Yeo <yeosamuel95@gmail.com>
2021-04-26 04:45:56 -0600

    ✅ Create test case for Key ABCD keys

--- test/key_test.exs
+++ test/key_test.exs
@@ -9,6 +9,10 @@ defmodule KeyTest do
   end
 
   test "It can make A B C D key out of a seed" do
+    seed = "01234"
+    abcd_keys = Key.get_abcd_keys(seed)
 
+    expected = %{a: "01", b: "12", c: "23", d: "34"}
+    assert abcd_keys === expected
   end
 end
