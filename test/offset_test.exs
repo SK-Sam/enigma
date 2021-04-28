b46fc73548b7a71182e6250266d7f788d19c32c1
Sam Yeo <yeosamuel95@gmail.com>
2021-04-28 04:50:07 -0600

    ✅ Create test for Offset module to square a date string

---
+++ test/offset_test.exs
@@ -0,0 +1,19 @@
+defmodule OffsetTest do
+  use ExUnit.Case
+  doctest Offset
+
+  test "It can square a string date" do
+    squared_string = Offset.date_string_squared("290198")
+
+    expectation = "84214879204"
+
+    assert squared_string == expectation
+  end
+
+  test "It can square today's date if there is no arg given" do
+    squared_string = Offset.date_string_squared
+
+    assert is_string(squared_string) == true
+    assert String.length(squared_string) > 0
+  end
+end
